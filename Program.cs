8704cf3684bac9d348af1260e1bcbd6a3060611d
Sérgio Lima <sergio.lima@lidya.info>
2022-11-28 09:24:49 +0000

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Hi_Low
+{
+    internal class Program
+    {
+        internal const int minimum = 0;
+        internal const int maximum = 100;
+
+        internal enum EGuess_Result
+        {
+            no_guess,
+            not_valid_number,
+            lower,
+            higher,
+            equal
+        }
+
+        public static void Main(string[] args)
+        {
+            int number_of_guesses = run_game();
+
+            Console.WriteLine($"User needed {number_of_guesses} to guess number");
+            Console.ReadLine();
+
+        }
+
+        private static int run_game()
+        {
+            int try_count = 0;
+            EGuess_Result guess_result = EGuess_Result.no_guess;
+            Random rand = new();
+
+            int target_number = rand.Next(minimum, maximum);
+
+            do
+            {
+                if (guess_result != EGuess_Result.not_valid_number)
+                {
+                    try_count++;
+                }
+
+                Console.WriteLine($"Please enter a number between {minimum} and {maximum}");
+                string number = Console.ReadLine() ?? "";
+
+                guess_result = check_guess(number, target_number);
+
+                switch (guess_result)
+                {
+                    case EGuess_Result.not_valid_number:
+                        Console.WriteLine("Not a valid number");
+                        break;
+                    case EGuess_Result.lower:
+                    case EGuess_Result.higher:
+                        Console.WriteLine($"Number is {guess_result} than target");
+                        break;
+                }
+
+            } while (guess_result != EGuess_Result.equal);
+            
+            return try_count;
+        }
+
+        public static EGuess_Result check_guess(string str_number, int target_number)
+        {
+            if (!int.TryParse(str_number, out int number))
+            {
+                return EGuess_Result.not_valid_number;
+            }
+
+            if (number < minimum || number > maximum)
+            {
+                return EGuess_Result.not_valid_number;
+            }
+
+            if (number > target_number)
+            {
+                return EGuess_Result.higher;
+            }
+
+            if (number < target_number)
+            {
+                return EGuess_Result.lower;
+            }
+
+            return EGuess_Result.equal;
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hi_Low
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void not_a_number()
+        {
+            Program.EGuess_Result response = Program.check_guess(Guid.NewGuid().ToString(), Program.maximum);
+
+            Assert.AreEqual(Program.EGuess_Result.not_valid_number, response);
+        }
+
+        [TestMethod]
+        public void lower_than_minimal()
+        {
+            Program.EGuess_Result response = Program.check_guess((Program.minimum - 1).ToString(), Program.maximum);
+
+            Assert.AreEqual(Program.EGuess_Result.not_valid_number, response);
+        }
+
+        [TestMethod]
+        public void higher_than_maximum()
+        {
+            Program.EGuess_Result response = Program.check_guess((Program.maximum + 1).ToString(), Program.maximum);
+
+            Assert.AreEqual(Program.EGuess_Result.not_valid_number, response);
+        }
+
+        [TestMethod]
+        public void higher_than_target()
+        {
+            Program.EGuess_Result response = Program.check_guess(Program.maximum.ToString(), Program.minimum);
+
+            Assert.AreEqual(Program.EGuess_Result.higher, response);
+        }
+
+        [TestMethod]
+        public void lower_than_target()
+        {
+            Program.EGuess_Result response = Program.check_guess(Program.minimum.ToString(), Program.maximum);
+
+            Assert.AreEqual(Program.EGuess_Result.lower, response);
+        }
+
+        [TestMethod]
+        public void equal_to_target()
+        {
+            int target = (Program.maximum + Program.minimum) / 2;
+            Program.EGuess_Result response = Program.check_guess(target.ToString(), target);
+
+            Assert.AreEqual(Program.EGuess_Result.equal, response);
+        }
+
+        [TestMethod]
+        public void equal_to_target_maximum_edge()
+        {
+            Program.EGuess_Result response = Program.check_guess(Program.maximum.ToString(), Program.maximum);
+
+            Assert.AreEqual(Program.EGuess_Result.equal, response);
+        }
+
+        [TestMethod]
+        public void equal_to_target_minimum_edge()
+        {
+            Program.EGuess_Result response = Program.check_guess(Program.minimum.ToString(), Program.minimum);
+
+            Assert.AreEqual(Program.EGuess_Result.equal, response);
+        }
+    }
+}
